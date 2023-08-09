09dd46fb3aa622802027813c86c50f7f52775123
vinodhconnects <vinodhconnects@gmail.com>
2023-08-09 11:45:10 +0530

    positional-parameter

--- src/main/java/com/restservices/controllers/FirstRestService.java
+++ src/main/java/com/restservices/controllers/FirstRestService.java
@@ -1,5 +1,6 @@
 package com.restservices.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,10 @@ public class FirstRestService {
 	@RequestMapping("/greet")
 	public String greet() {
 		 return " Spring boot is running";
+	}
+	
+	@RequestMapping("/greet/{name}") //Path variables or positional Paramater
+	public String greet(@PathVariable  String name) {
+		 return "Hi!!! "+name;
 	}
 }
