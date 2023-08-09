f3f0fe2356275469569f67e5d78c7f9ee1097c2b
vinodhconnects <vinodhconnects@gmail.com>
2023-08-09 11:28:13 +0530

    first-commit

--- src/main/java/com/restservices/FirstRestServiceApplication.java
+++ src/main/java/com/restservices/FirstRestServiceApplication.java
@@ -2,8 +2,10 @@ package com.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = "com.restservices.controllers")
 public class FirstRestServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/restservices/controllers/FirstRestService.java
+++ src/main/java/com/restservices/controllers/FirstRestService.java
@@ -1,5 +1,14 @@
 package com.restservices.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class FirstRestService {
 
+	@RequestMapping("/greet")
+	public String greet() {
+		 return " Spring boot is running";
+	}
 }
