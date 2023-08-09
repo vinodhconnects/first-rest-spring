6f41d1ad97b472711978ec7808c0dbfa4fffd814
vinodhconnects <vinodhconnects@gmail.com>
2023-08-09 12:20:34 +0530

    service-commit

--- src/main/java/com/restservices/services/DataService.java
+++ src/main/java/com/restservices/services/DataService.java
@@ -1,6 +1,14 @@
 package com.restservices.services;
 
+import org.springframework.stereotype.Service;
+
 @Service
 public class DataService {
+	
+	String names[]= {"Joseph","Arjun","Naveen","Peterson","Mary"};
+	
+	public String[] getNames() {
+		return names;
+	}
 
 }
