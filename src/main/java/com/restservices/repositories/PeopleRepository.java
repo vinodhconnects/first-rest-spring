46c89eb97da8f49065593ce59907aabe36f3543f
vinodhconnects <vinodhconnects@gmail.com>
2023-08-10 11:25:13 +0530

    peopleRepository

--- src/main/java/com/restservices/repositories/PeopleRepository.java
+++ src/main/java/com/restservices/repositories/PeopleRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.restservices.models.People;
 
 public interface PeopleRepository  extends JpaRepository<People, Long>{
-
+   public People findBySno(Integer sno); //this method will be implemented by data jpa
+                                        //but convention must find +By+field name
 }
