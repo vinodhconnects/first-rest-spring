04f1a6eea41e98a19ffdfe80968c07a7d8f7023c
vinodhconnects <vinodhconnects@gmail.com>
2023-08-10 10:08:29 +0530

    full-flow

--- src/main/java/com/restservices/controllers/PeopleAPI.java
+++ src/main/java/com/restservices/controllers/PeopleAPI.java
@@ -1,10 +1,24 @@
 package com.restservices.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.restservices.models.People;
+import com.restservices.services.PeopleService;
 
 @RestController
 @RequestMapping("/api")
 public class PeopleAPI {
 
+	@Autowired
+	private PeopleService ps;
+	
+	@GetMapping("/people")
+	public List<People> getPeople() {
+		return ps.getPeople();
+	}
 }
