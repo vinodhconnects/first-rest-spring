0f7a0bd8a945262743412990663594cd9e87d166
vinodhconnects <vinodhconnects@gmail.com>
2023-08-10 09:44:51 +0530

    peopleapi

--- src/main/java/com/restservices/controllers/PeopleAPI.java
+++ src/main/java/com/restservices/controllers/PeopleAPI.java
@@ -1,5 +1,10 @@
 package com.restservices.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class PeopleAPI {
 
 }
