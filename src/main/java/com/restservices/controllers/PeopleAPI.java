c626e363e24f01655a29e03979694411d7ab7076
vinodhconnects <vinodhconnects@gmail.com>
2023-08-10 10:29:16 +0530

    peopleapi

--- src/main/java/com/restservices/controllers/PeopleAPI.java
+++ src/main/java/com/restservices/controllers/PeopleAPI.java
@@ -3,7 +3,11 @@ package com.restservices.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +24,16 @@ public class PeopleAPI {
 	@GetMapping("/people")
 	public List<People> getPeople() {
 		return ps.getPeople();
+	}
+	
+	@PostMapping("/people")
+	public ResponseEntity<People> addPerson(@RequestBody People people) {
+		try {
+			ps.addPerson(people);
+			return new ResponseEntity<People>(people,HttpStatus.OK);
+		}
+		catch(Exception e) {
+			return new ResponseEntity<People>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
