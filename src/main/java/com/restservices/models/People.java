656d739cc8babb5ee3229d2e8ed3453225e5da14
vinodhconnects <vinodhconnects@gmail.com>
2023-08-10 09:53:20 +0530

    repo

--- src/main/java/com/restservices/models/People.java
+++ src/main/java/com/restservices/models/People.java
@@ -1,5 +1,7 @@
 package com.restservices.models;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -9,6 +11,11 @@ import jakarta.persistence.Table;
 @Table
 public class People {
 
+	/**
+	 * 
+	 */
+
+	
 	@Id
 	 private Integer sno;
 	@Column
---
+++ src/main/java/com/restservices/repositories/PeopleRepository.java
@@ -0,0 +1,5 @@
+package com.restservices.repositories;
+
+public interface PeopleRepository {
+
+}
