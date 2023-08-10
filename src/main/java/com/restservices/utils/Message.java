96360fa612983bbac2c382ad94ef9a1361b4a558
vinodhconnects <vinodhconnects@gmail.com>
2023-08-10 11:36:56 +0530

    message-class

---
+++ src/main/java/com/restservices/utils/Message.java
@@ -0,0 +1,18 @@
+package com.restservices.utils;
+
+public class Message {
+  private String status;
+  
+  public Message() {}
+  public Message(String status) {
+	  this.status =status;
+  }
+public String getStatus() {
+	return status;
+}
+public void setStatus(String status) {
+	this.status = status;
+}
+  
+  
+}
