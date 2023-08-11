4b0a77186832a120f76560ce3e592b1858405689
vinodhconnects <vinodhconnects@gmail.com>
2023-08-11 10:07:18 +0530

    test-case

--- src/test/java/com/restservices/FirstRestServiceApplicationTests.java
+++ src/test/java/com/restservices/FirstRestServiceApplicationTests.java
@@ -1,13 +1,34 @@
 package com.restservices;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.restservices.controllers.PeopleAPI;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class FirstRestServiceApplicationTests {
 
+	@Autowired
+	private PeopleAPI api;
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
 	@Test
 	void contextLoads() {
+	   assertThat(api).isNotNull()	;
+	}
+	
+	@Test
+	public void checkThis() throws Exception {
+		mockMvc.perform(get("/api/people")).andExpect(status().isOk());
 	}
 
 }
